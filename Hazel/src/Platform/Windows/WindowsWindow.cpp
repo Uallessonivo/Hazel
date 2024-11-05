89d0f433be650658b104536bb598d489f96c3ddb
Uallessonivo <uallessons@gmail.com>
2024-11-05 17:40:47 -0300

    Working on Window interface and WindowsWindow implementation

---
+++ Hazel/src/Platform/Windows/WindowsWindow.cpp
@@ -0,0 +1,25 @@
+#include "hzpch.h"
+#include "WindowsWindow.h"
+
+namespace Hazel {
+
+	static bool s_GLFWInitialized = false;
+
+	Window* Window::Create(const WindowProps& props)
+	{
+		return new WindowsWindow(props);
+	}
+
+	WindowsWindow::WindowsWindow(const WindowProps& props)
+	{
+		Init(props);
+	}
+
+	WindowsWindow::~WindowsWindow()
+	{}
+
+	void WindowsWindow::Init(const WindowProps& props)
+	{
+
+	}
+}
