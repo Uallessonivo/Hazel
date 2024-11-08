681a13885bbc4609682194c6e4599356e2333e84
Uallessonivo <uallessons@gmail.com>
2024-11-08 15:34:59 -0300

    Add event handling and GLFW callbacks to Application

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,15 +1,26 @@
 #include "hzpch.h"
+#include "Log.h"
 #include "Application.h"
 #include "GLFW/glfw3.h"
 
 namespace Hazel {
+#define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
-		m_Window = std::unique_ptr<Window>(Window::Create());	
+		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 	}
 
 	Application::~Application()
 	{
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		HZ_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -20,5 +31,11 @@ namespace Hazel {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
