33dfa2e2d32163bfb3ba35d38a785d016f3b5acb
Uallessonivo <uallessons@gmail.com>
2024-11-09 17:20:04 -0300

    Make m_Handled public and initialize to false

--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -38,6 +38,8 @@ namespace Hazel {
 	{
 		friend class EventDispatcher;
 	public:
+		bool m_Handled = false;
+
 		virtual EventType GetEventType() const = 0;
 		virtual const char* GetName() const = 0;
 		virtual int GetCategoryFlags() const = 0;
@@ -47,9 +49,6 @@ namespace Hazel {
 		{
 			return GetCategoryFlags() & category;
 		}
-
-	protected:
-		bool m_Handled = false;
 	};
 
 	class EventDispatcher
