46830ba6608efe404ddafd9eb274611bceb7f5cf
Uallessonivo <uallessons@gmail.com>
2024-11-14 11:08:55 -0300

    Rename GetWeight to GetHeight in WindowResizeEvent

--- Hazel/src/Hazel/Events/ApplicationEvent.h
+++ Hazel/src/Hazel/Events/ApplicationEvent.h
@@ -10,7 +10,7 @@ namespace Hazel {
 		WindowResizeEvent(unsigned int width, unsigned int height) : m_Width(width), m_Height(height) {}
 
 		unsigned int GetWidth() const { return m_Width; }
-		unsigned int GetWeight() const { return m_Height; }
+		unsigned int GetHeight() const { return m_Height; }
 
 		std::string ToString() const override
 		{
